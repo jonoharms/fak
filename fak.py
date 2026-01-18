154b2ccdac3c23ce4962c3c38c3de2ea881b3b24
Jonathon Harms <jonoharms@gmail.com>
2026-01-18 22:51:57 +1100

    fix

--- fak.py
+++ fak.py
@@ -8,7 +8,10 @@ import sys
 import time
 import shutil
 
-os.chdir(sys.path[0])
+try:
+    os.chdir(sys.path[0])
+except FileNotFoundError:
+    pass
 
 EVAL_PATH = '.eval.json'
 BUILD_DIR = 'build'
@@ -90,9 +93,10 @@ def meson_configure():
     if result['__hash__'] == HASH_MANAGED:
         print("Info: This is a managed evaluation.")
 
-    with open('meson_opts.txt', 'w') as f:
-        for key, value in result['defines'].items():
-            f.write(f'-D{key}={value}\n')
+    if 'defines' in result:
+        with open('meson_opts.txt', 'w') as f:
+            for key, value in result['defines'].items():
+                f.write(f'-D{key}={value}\n')
 
     for key, value in result['meson_options'].items():
         subprocess.run(['meson', 'configure', f'-D{key}={value}'], check=True, cwd=BUILD_DIR)
